0b625acd574cfa6ce84be7efdedf02b013d2ecab
Mees Fix <mfix@stsci.edu>
2021-02-16 14:55:27 -0500

    fixed test

--- test_repo.py
+++ test_repo.py
@@ -5,6 +5,12 @@ import numpy as np
 def test_square_table():
     """ Test square of x column"""
     data = ascii.read('data.txt')
-    source = data['x']**2
+    source = data['x'].data**2
     truth = np.array([1, 2, 3, 4, 5])**2
-    assert np.testing.assert_array_equal(source, truth)
+
+    comparison = source == truth
+    equal_arrays = comparison.all()
+
+    assert equal_arrays
+
+test_square_table()
